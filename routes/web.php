eb18a91361c895e2df5de296f3d1b7ae3810047b
Alemayehu Abebe <alexsabebe28@gmail.com>
2025-07-08 10:00:40 +0300

    2017 01 10

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,25 +11,7 @@ Route::get('/about', function(){
   echo "Hey am about us Page Know";
 })->name('about');
 
-Route::get('/user/{id}/{slug}', function($id, $slug){
-    echo "Welcome user :".$id . "With Slug :" . $slug;
-})->name('user');
+Route::resource('blog', BlogController::class);
 
 
-//grouping the routes
 
-Route::group(['prefix'=> 'blog', 'as'=>'blog.'], function(){
-   Route::get('/create', function(){
-        echo "create blog";
-   })->name('create');
-   Route::get('/update', function(){
-        echo "update blog";
-   })->name('update');
-   Route::get('/show', function(){
-        echo "show blog";
-   })->name('show');
-});
-
-Route::fallback(function (){
-   return "Oops page not found";
-});
