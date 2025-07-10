23388d4b17f275a7be0d9fb890c2348f233ada1d
Alemayehu Abebe <alexsabebe28@gmail.com>
2025-07-10 09:28:18 +0300

    this query builder practice

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,13 +7,35 @@ use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     function index(){
-        DB::table('users')->insert(
-            [
-            'name' => 'Alesa',
-            'email' => 'alexsabebe@gmail.com',
-            'password' => '12345678'
-            ]
-        );
+        //insert new data to data base by query builder
+        // DB::table('users')->insert(
+        //     [
+        //     'name' => 'Alesa',
+        //     'email' => 'alexsabebe@gmail.com',
+        //     'password' => '12345678'
+        //     ]
+        // );
+
+        //get the data from the user table
+
+        // $users = DB::table('users')->where('id',2)->first();
+        // return $users;
+
+        //update the  data
+
+        // DB::table('users')->where('id',2)->update([
+        //     'name'=> 'Tomas Abeba',
+        // ]
+        // );
+
+        //Delete row data from data base
+
+        // DB::table('users')->where('id',2)->delete();
+
+        //
+        $blogs = DB::table('blogs')->pluck('title')->toArray();
+        dd($blogs);
+
         return view('welcome');
     }
 }
